300d2eb1206ba07013befe25838ca0240e55c594
LorenzoPecorari <pecorari.1885161@studenti.uniroma1.it>
2022-07-31 22:10:31 +0200

    fix vari

--- cli/top.c
+++ cli/top.c
@@ -2,8 +2,8 @@
 
 // variabili globali
 int num;
-float uptime;
-float cpu_percentage;
+long unsigned uptime;
+double cpu_percentage;
 long unsigned memory;
 long int hertz;
 proc procs[MAX_PROCESSES];
@@ -18,7 +18,6 @@ void handle_error(const char* msg){
 void get_memory(){
 	char path[] = "/proc/meminfo";
 	int fd;
-	
 	if(!(fd = open(path, O_RDONLY, 0666)))
 		handle_error("Errore nell'apertura di fd per meminfo");
 		
@@ -50,7 +49,7 @@ void get_memory(){
 // rimuove le parentesi dal nome preso da stat
 void remove_parenthesis(char* s){
 	if(!s)
-		handle_error("Riferimento a stringa nullo");
+		handle_error("Riferimento nullo a stringa");
 		
 	int len = strlen(s);
 	
@@ -85,7 +84,7 @@ void get_uptime(struct dirent* dir){
     while(read(fd, &time[s], 1) && s < 16)
         s++;
 
-    uptime = atof(time);  
+    uptime = atol	(time);  
 
     if(close(fd) == -1)
         handle_error("Errore nella chiusura di uptime");
@@ -93,16 +92,106 @@ void get_uptime(struct dirent* dir){
     return;
 }
 
-// analizza stat del processo per ricavarne informazioni
+// analisi stat e statm del processo per ricavarne informazioni
 /*
-	stats[1] -> nome del processo
-	stats[4] -> stato del processo
-	stats[13] -> tempo in user mode
-	stats[14] -> tempo in kernel mode
-	stats[17]  + stats[18] -> tempo processi figli
-	stats[24] -> tempo avvio processo dopo avvio os
+	- stats[1] -> nome del processo
+	- stats[4] -> stato del processo
+	- stats[13] -> tempo in user mode
+	- stats[14] -> tempo in kernel mode
+	- (stats[17]  + stats[18]) -> tempo processi figli
+	- stats[21] -> tempo di start
+	- stats[24] -> tempo avvio processo dopo avvio os
+	- statm[0] -> totale memoria virtuale del processo
+	- statm[1] -> memoria processo residente nella ram
 */
-void get_stat(const char* path){
+
+//legge  informazioni da /proc/[PID]/stat
+void get_stat(const char* path_to_stat){
+	int fd, i, idx, inner_idx;
+    
+    if((fd = open(path_to_stat, O_RDONLY, 0666)) == -1)
+        handle_error("Errore nell'apertura di fd per stat");    
+
+    char stats[64][32];
+    char temp;
+    
+    for(i = 0; i < 64; i++){
+        memset(stats[i], 0, 32);
+	}
+	
+	idx = 0;
+    inner_idx = 0;
+    temp = 0;
+    
+    while(read(fd, &temp, 1) && idx < 64 && inner_idx < 32){
+        if(temp == 32){
+            inner_idx = 0;
+            idx++;
+        }
+        else{
+            stats[idx][inner_idx] = temp;
+            inner_idx++;
+            }
+    }
+    
+    if(close(fd) == -1){
+    	handle_error("Errore nella chiusura di fd per stat");
+    }
+    
+	strcpy(procs[num].name, stats[1]);
+    strcpy(&procs[num].status, stats[4]);
+	procs[num].utime = atol(stats[13]);
+    procs[num].stime = atol(stats[14]);
+    procs[num].children_time = atol(stats[15]) + atol(stats[16]);
+    procs[num].tot_time = procs[num].utime + procs[num].stime + procs[num].children_time;
+    procs[num].starttime = atol(stats[21]);
+    procs[num].mem_usage = atol(stats[23]);
+    
+	remove_parenthesis(procs[num].name);
+
+	return;
+}
+
+// legge informazioni da /proc/[PID]/statm
+// (ridondante, possibile rimuoverla, stesse info prese da stat)
+void get_statm(const char* path_to_statm){
+	int fd, i, idx, inner_idx;
+	
+    if((fd = open(path_to_statm, O_RDONLY, 0666)) == -1){
+        handle_error("Errore nell'apertura di fd per statm");   
+	}
+	
+	char temp;
+	char statm[7][16];
+    
+    for(i = 0; i < 7; i++){
+		memset(statm[i], 0, 16);
+    }
+
+	idx = 0;
+	inner_idx = 0;
+	temp = 0;
+	
+	while(read(fd, &temp, 1) && idx < 7 && inner_idx < 16){
+        if(temp == 32){
+            inner_idx = 0;
+            idx++;
+        }
+        else{
+            statm[idx][inner_idx] = temp;
+            inner_idx++;
+            }
+    }
+
+	if(close(fd) == -1)
+		handle_error("Errore nella chiusura di fd per statm");
+    
+    procs[num].mem_usage = atol(statm[1]);
+
+	return;
+}
+
+void get_stats(const char* path){
 	if(!path)
 		handle_error("Errore nel passaggio del percorso");
 		
@@ -122,88 +211,12 @@ void get_stat(const char* path){
     strcat(path_to_statm, path);
     strcat(path_to_statm, "/statm");
 
-    int fd;
-    
-    //lettura informazioni da /proc/[PID]/stat
-    if((fd = open(path_to_stat, O_RDONLY, 0666)) == -1)
-        handle_error("Errore nell'apertura di fd per stat");    
-
-    char stats[64][32];
-    
-    int i = 0;
-    
-    for(; i < 64; i++){
-        memset(stats[i], 0, 32);
-	}
+	get_stat(path_to_stat);
+	//get_statm(path_to_statm);
 	
-	int idx = 0;
-    int inner_idx = 0;
+	procs[num].load_percentage = (double) (procs[num].tot_time / hertz) / (uptime - (procs[num].starttime / hertz)) * 100;
+    cpu_percentage += procs[num].load_percentage;
     
-    char temp[1];
-    temp[0] = 0;
-
-    while(read(fd, &temp[0], 1) && idx < 64 && inner_idx < 32){
-        //temp[1] = 0;
-        if(temp[0] == 32){
-            inner_idx = 0;
-            idx++;
-        }
-        else{
-            stats[idx][inner_idx] = temp[0];
-            inner_idx++;
-            }
-    }
-    
-    if(close(fd) == -1)
-    	handle_error("Errore nella chiusura di fd per stat");
-    
-    //lettura informazioni da /proc/[PID]/statm
-    char statm[7][16];
-    
-    for(i = 0; i < 7; i++){
-		memset(statm[i], 0, 16);
-    }
-    
-    if((fd = open(path_to_statm, O_RDONLY, 0666)) == -1){
-        handle_error("Errore nell'apertura di fd per statm");   
-	}
-
-	idx = 0;
-	inner_idx = 0;
-	
-	while(read(fd, &temp[0], 1) && idx < 7 && inner_idx < 16){
-        if(temp[0] == 32){
-            inner_idx = 0;
-            idx++;
-        }
-        else{
-            statm[idx][inner_idx] = temp[0];
-            inner_idx++;
-            }
-    }
-
-	if(close(fd) == -1)
-		handle_error("Errore nella chiusura di fd per statm");
-
-	// popolazione dell struttura
-	strcpy(procs[num].name, stats[1]);
-    strcpy(&procs[num].status, stats[4]);
-	procs[num].utime = atoi(stats[13]);
-    procs[num].stime = atoi(stats[14]);
-    procs[num].children_time = atoi(stats[17]) + atoi(stats[18]);
-    procs[num].tot_time = procs[num].utime + procs[num].stime + procs[num].children_time;
-    procs[num].starttime = atoi(stats[21]);
-    
-	remove_parenthesis(procs[num].name);
-    
-    procs[num].mem_usage = atoi(statm[0]);
-    
-    /*
-    procs[num].load_percentage = (((procs[num].stime + procs[num].utime) / hertz) / (uptime - (procs[num].starttime / hertz))) * 100;
-    cpu_percentage += procs[num].load_percentage; 
-	*/
-	
-	procs[num].load_percentage = (procs[num].tot_time / hertz) / (uptime - (procs[num].starttime / hertz)) * 100;
     cpu_percentage += procs[num].load_percentage;
     	
 	return;
@@ -245,7 +258,7 @@ void insert_process(struct dirent* d){
 
     char cmdline_buf[BUF_SIZE];
     get_cmdline(path_to_cmdline, cmdline_buf);
-    get_stat(path);
+    get_stats(path);
 
 	procs[num].path = d->d_name;
 	strcat(procs[num].cmdline, cmdline_buf);
@@ -262,18 +275,20 @@ void insert_process(struct dirent* d){
 	}
 
 	num++;
+	
+	return;
 }
 
 // stampa i processi
 void print_processes(){
 	int i = 0;
 	
-	printf("No. processes : %d - Uptime : %.2lf - Load : %.2lf %c\n", num, uptime, cpu_percentage, '%');
+	printf("No. processes : %d - Uptime : %.2ld - Load : %.2lf %c\n", num, uptime, cpu_percentage, '%');
 	
 	printf("# PID  - NAME    - CMDLINE    \t- STARTTIME    \t- TIME - MEMORY_LOAD \t - CPU LOAD\n");
 	for(; i < num; i++)
-		printf("# %s - %s - %s - %d - %d - %d - %.2f %c\n", procs[i].path, procs[i].name, procs[i].cmdline, procs[i].starttime, procs[i].tot_time, procs[i].mem_usage, procs[i].load_percentage, '%');
-		
+		printf("# %s - %s - %s - %lld - %ld - %ld - %.2lf %c\n", procs[i].path, procs[i].name, procs[i].cmdline, procs[i].starttime, procs[i].tot_time, procs[i].mem_usage, procs[i].load_percentage, '%');
+	
 	printf("\n");
 	return;
 }
--- cli/top.h
+++ cli/top.h
@@ -20,21 +20,21 @@ typedef struct proc{
 	char name[NAME_SIZE];
 	char cmdline[BUF_SIZE];
 	char status;
-	int starttime;
-	int utime;
-	int stime;
-	int children_time;
-	int tot_time;
-	int mem_usage;
-    float load_percentage;
+	long long unsigned starttime;
+	long unsigned utime;
+	long unsigned stime;
+	long unsigned children_time;
+	long unsigned tot_time;
+	long unsigned mem_usage;
+    double load_percentage;
     float mem_percentage;
 } proc;
 
 // variabili globali
 extern int num;
 extern long int hertz;
-extern float uptime;
-extern float cpu_percentage;
+extern long unsigned uptime;
+extern double cpu_percentage;
 extern long unsigned memory;
 extern proc procs[MAX_PROCESSES];
 
@@ -44,7 +44,9 @@ void get_memory();
 void insert_process(struct dirent* d);
 void remove_parenthesis(char* s);
 void get_uptime(struct dirent* d);
-void get_stat(const char* path);
+void get_stat(const char* path_to_stat);
+void get_statm(const char* path_to_statm);
+void get_stats(const char* path);
 void get_cmdline(const char* directory, char buf[]);
 void print_processes();
 
